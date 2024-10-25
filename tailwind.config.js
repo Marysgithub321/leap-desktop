c9e06dfe1664c27218d1b8ff88db876cbaa94eb0
Maria Peters <mp-developer@outlook.com>
2024-10-25 09:33:51 -0600

    updated the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGray: '#3B3A3D',
+        lightGray: '#E5E5E5',
+        teal: '#319795',
+        darkBlue: '#443c5b',
+        pink: '#f8cacd',
+        tealLight: '#7fbaa8',
+        green: '#7ec682',
+        blue: '#0097b2',
+      },
+    },
   },
   plugins: [],
-}
-
+};
